1dd800093520f74d46d0258967651a8bc88e614b
joshua <joshuatipan10@gmail.com>
2024-07-10 01:07:22 +0800

    making responsive design for mobile is fucking hard FUCK!

--- src/Nav-routes/nav.jsx
+++ src/Nav-routes/nav.jsx
@@ -1,24 +1,26 @@
 import styles from './nav.module.css'
 
 
-
-
-
-
 function Nav(){
     return ( 
-        <>    
-            
+        <>     
             <div className={styles.navMoveSide}>
-            <ul className={styles.navUl}>
-                <h2 className={styles.byname}>by.joshua</h2>
-                <li className={styles.navList}><a >About Me</a></li>
-                <li className={styles.navList}><a >PortFolio</a></li>
-                <li className={styles.navList}><a >Contact</a></li>
-            </ul>
+                <ul className={styles.navUl}>
+                    <h2 className={styles.byname}>by.joshua</h2>
+                    <div className={styles.menu}>
+                        <li className={styles.navList}>
+                            <div className={styles.line}></div>
+                            <div className={styles.line}></div>
+                            <div className={styles.line}></div>
+                        </li>
+                        <li className={styles.navList}><a >About Me</a></li>
+                        <li className={styles.navList}><a >PortFolio</a></li>
+                        <li className={styles.navList}><a >Contact</a></li>
+                    </div>
+
+                </ul>
             </div>
         </>
     );
 }
-
 export default Nav
