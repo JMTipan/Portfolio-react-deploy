0c09c2a2a6d8dfde6e382a76edcec12c1188dbb8
joshua <joshuatipan10@gmail.com>
2024-07-09 22:06:32 +0800

    i fix something

--- src/home/hero.jsx
+++ src/home/hero.jsx
@@ -11,17 +11,21 @@ function Hero(props){
     return (
         <>
         <div className={style.heroFlexContainer}>
-            <div className={style.hero}>
-                <h1>Hi, Im Joshua im a front end developer</h1>
-                <p>i still dont kow what to put here :)</p>
+            <div className={style.hero_techStack}>
+                <div className={style.hero}>
+                    <h1>Hi, Im Joshua im a front end developer</h1>
+                    <p>i still dont kow what to put here :)</p>
+                </div>
+                <div className={style.techStackContainer}>
+                    <img src={javascritp_logo} alt="javascript" width={50}/>
+                    <img src={html_logo} alt="html" width={50}/>
+                    <img src={css_logo} alt="css" width={50}/>
+                    <img src={react_logo} alt="react" width={50}/>
+                </div>
             </div>
+
             <img className={style.heroProfile} src={among_us} alt="" height={400} width={400}/>
-            <div className={style.techStackContainer}>
-                <img src={javascritp_logo} alt="javascript" width={50}/>
-                <img src={html_logo} alt="html" width={50}/>
-                <img src={css_logo} alt="css" width={50}/>
-                <img src={react_logo} alt="react" width={50}/>
-            </div>
+
         </div>
 
           
